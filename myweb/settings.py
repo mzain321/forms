3cbf2db8af48d1ce0fa6d8392c35b2b227209f71
Muhammad Zain <zain0909012@gmail.com>
2022-09-09 12:09:43 +0500

    Update settings.py

--- myweb/settings.py
+++ myweb/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-m!syqk)=^q-ecxx+p%c%g=wp%lz9rqxxra6p)^9c_-g_!w)b34
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['customformbyz.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['zainforms.herokuapp.com','127.0.0.1']
 
 
 # Application definition

a65295665661e1bdeaa6744fc7aea536982c69e7
Muhammad Zain <zain0909012@gmail.com>
2022-09-13 14:03:35 +0500

    Update urls.py

--- base/urls.py
+++ base/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
 
 	path('',views.homenew),
 	path('thank you page.html',views.thankyou),
-	path('form2',views.home2),
+	path('tabeeb',views.home2),
 	path('new',views.home3),
 	path('old',views.home)
 
